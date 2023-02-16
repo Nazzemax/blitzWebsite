b1b3971b1c71fdc4e3d8aecde2a4f46d167f749f
Nazzemax <hvan1998@inbox.ru>
2023-02-16 16:45:06 +0600

    Desktop only revsion of website Blitz

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+$(function(){
+
+$('.header-slider__wrapper').slick({
+	arrows:false,
+	dots:true,
+	dotsClass:'header-dots',
+	autoplay:true,
+});
+
+
+$('.projects__slider').slick({
+	arrows:false,
+	dots:true,
+	dotsClass:'projects-dots',
+	autoplay:true,
+});
+
+$('.blog__slider').slick({
+	arrows:false,
+	dots:true,
+	dotsClass:'blog-dots',
+	autoplay:true,
+});
+
+
+});
